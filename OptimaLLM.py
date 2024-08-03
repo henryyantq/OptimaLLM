b357f710d7b4650a92a9c9c33d17f81f6bba5dc8
Evergardener <henry0927@outlook.com>
2024-08-03 21:44:03 +0800

    Update OptimaLLM.py

--- OptimaLLM.py
+++ OptimaLLM.py
@@ -1,88 +1,95 @@
 import httpx
 
-OPENAI_API_KEY = 'YOUR_OPENAI_API_KEY'
-MODEL = 'gpt-3.5-turbo'
+API_KEY = 'YOUR_API_KEY'
+MODEL = 'gpt-4o'
 OPTIMODE = 'original'
 
-def send_req_receive(message, model=MODEL):
-    url = 'https://api.openai.com/v1/chat/completions'
+def chat(message, model=MODEL, temperature=0.8):
+    url = 'https://api.chatai.beauty/v1/chat/completions'
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {OPENAI_API_KEY}'
+        'Authorization': f'Bearer {API_KEY}'
     }
     data = {
         'model': model, 
-        'messages': message
+        'messages': message,
+        'temperature': temperature
     }
-    r = httpx.post(url, headers=headers, json=data, timeout=5000)
-    return r.json()['choices'][0]['message']
+    while True:
+        try:
+            r = httpx.post(url, headers=headers, json=data, timeout=200)
+            return r.json()['choices'][0]['message']
+        except:
+            continue
 
 
-while (1):
-    init_prompt = input('\nPlease type in your question here: ')
-    max_iters = input('Please type in the maximum iterations of the enhancement: ')
-
-    message = [
-        {'role': 'user', 'content': init_prompt}
-        ]
-    
-    received_content = send_req_receive(message=message)
-    print('\033[33mThe original first-attempt answer >> ', received_content['content'], '\033[0m\n')
+def refiner(init_prompt, max_iters, model=MODEL, temperature=0.8):
+    message = [{'role': 'user', 'content': init_prompt}]
+    received_content = chat(
+        message=message, 
+        model=model, 
+        temperature=temperature
+    )
     first_attempt = received_content['content']
-
     iter_count = 0
     answers = []
-
-    for iter in range(0, int(max_iters)):
+    for _ in range(0, int(max_iters)):
         iter_count += 1
         former_feedback = received_content
-
         flaw_indicating = {'role': 'user',
                            'content': 'Please list the defects of answer "' \
                             + former_feedback['content'] + '" to the question "' + init_prompt + '".' \
                                 + ' List the defects in one sentence instead of a list with line breaks!'}
-        flaw = send_req_receive(message=[flaw_indicating])
-        # print('\n\033[34m' + flaw['content'] + '\033[0m')
-
-        interm_prompt = {'role': 'user', 
-                        'content': 'The answer "' + former_feedback['content'] \
-                            + '" to the question "' + init_prompt \
-                                + '" is not optimal because that' + flaw['content'] \
-                                    + ' Please refine the answer by providing a better one regarding the aforementioned flaw.' \
-                                        + ' You should provide nothing else but the answer.'}
-        # print('\n\033[34m' + interm_prompt['content'] + '\033[0m')
-        
-        received_content = send_req_receive(message=[interm_prompt])
-        print('\n\033[34m' + received_content['content'] + '\033[0m')
+        flaw = chat(message=[flaw_indicating])
+        interm_prompt = [{
+            'role': 'user',
+            'content': 'The answer "' + former_feedback['content'] \
+                + '" to the question "' + init_prompt \
+                    + '" is not optimal because that' + flaw['content'] \
+                        + ' Please refine the answer by providing a better one regarding the aforementioned flaw.' \
+                            + ' You should provide nothing else but the answer.'
+        }]
+        received_content = chat(
+            message=interm_prompt,
+            model=model, 
+            temperature=temperature
+        )
         answers.append(received_content['content'])
-
         if iter_count > 1:
-            judge_prompt = {'role': 'user', 
-                            'content': 'The question is "' + init_prompt + '", to which there are two optional answers, one is "' \
-                                + former_feedback['content'] + '", the other one is "' + received_content['content'] \
-                                    + '". Please answer "1" or "2" only if you think any one of them is better, or "0" only if you think they\'re equally good. Do not reply anything else than the number!'}
-            # print('\n\033[34m' + judge_prompt['content'] + '\033[0m')
-            
-            reward = send_req_receive(message=[judge_prompt])
-            # print('\n\033[34m' + 'reward for iter {} goes to '.format(iter_count) + reward['content'] + '\033[0m')
-            
+            judge_prompt = [{
+                'role': 'user',
+                'content': 'The question is "' + init_prompt + '", to which there are two optional answers, one is "' \
+                      + former_feedback['content'] + '", the other one is "' + received_content['content'] \
+                        + '". Please answer "1" or "2" only if you think any one of them is better, or "0" only if you think they\'re equally good. Do not reply anything else than the number!'
+            }]
+            reward = chat(
+                message=judge_prompt,
+                model=model, 
+                temperature=temperature
+            )
             if reward['content'] == '1' or reward['content'] == '0':
                 received_content = former_feedback
                 break
-
-    print('\n\033[32mThe enhanced answer after {} iterations >> '.format(iter_count), received_content['content'], '\033[0m')
     enhanced = received_content['content']
+    return first_attempt, answers, enhanced
 
-    with open(init_prompt + '.txt', 'w', encoding='utf-8') as file:
-        file.write('[' + MODEL + ' - ' + OPTIMODE + ']\n\n')
-        file.write('Initial answer: ' + first_attempt + '\n')
-        
-        count = 0
-        
+
+def main():
+    while (1):
+        init_prompt = input('\nPlease type in your question here: ')
+        max_iters = input('\nPlease type in the maximum iterations of the enhancement: ')
+        first_attempt, answers, enhanced = refiner(
+            message=init_prompt,
+            max_iters=max_iters
+        )
+        trace = f'## Initial answer: \n\n{first_attempt}'
+        count = 1
         for ans in answers:
             count += 1
-            file.write('\nIntermediate answer {}: '.format(count) + ans)
+            trace += f'\n\n---\n## Intermediate answer {count}:\n\n{ans}'
+        trace += f'\n\n---\n## Enhancement result\n\n{enhanced}'
+        with open('result.md', 'w', encoding='utf-8') as f:
+            f.write(trace)
         
-        file.write('\n\nEnhancement result: ' + enhanced)
-        
-        
+
+main()
